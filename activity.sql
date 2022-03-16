45c6f307d0bad831ad45e1f464e6a46bd147db72
Faye <fayebheevayne@gmail.com>
2022-03-16 21:21:20 +0800

    added activity code

--- activity.sql
+++ activity.sql
@@ -59,4 +59,27 @@ CREATE TABLE posts_comments (
     	ON DELETE RESTRICT    
 );
 
+-- insert values in users table
+INSERT INTO users(email, password, datetime_created) VALUES ("johnsmmith@gmail.com", "passwordA", "2021-1-1 01:00:00");
+INSERT INTO users(email, password, datetime_created) VALUES ("juandelacruz@gmail.com", "passwordB", "2021-1-1 02:00:00");
+INSERT INTO users(email, password, datetime_created) VALUES ("janesmmith@gmail.com", "passwordC", "2021-1-1 03:00:00");
+INSERT INTO users(email, password, datetime_created) VALUES ("mariadelacruz@gmail.com", "passwordD", "2021-1-1 04:00:00");
+INSERT INTO users(email, password, datetime_created) VALUES ("johndoe@gmail.com", "passwordE", "2021-1-1 05:00:00");
 
+-- insert values in posts table
+INSERT INTO posts(user_id, title, content, datetime_posted) VALUES ( 1, "First Code", "Hello World!", "2021-1-2 01:00:00");
+INSERT INTO posts(user_id, title, content, datetime_posted) VALUES ( 1, "Second Code", "Hello Earth!", "2021-1-2 02:00:00");
+INSERT INTO posts(user_id, title, content, datetime_posted) VALUES ( 2, "Third Code", "Welcome to Mars!", "2021-1-2 03:00:00");
+INSERT INTO posts(user_id, title, content, datetime_posted) VALUES ( 4, "Fourth Code", "Bye bye solar system!", "2021-1-2 04:00:00");
+
+-- Get all the title with a user ID of 1
+SELECT title FROM posts WHERE user_id = 1;
+
+-- Get all user's email and datetime of creation
+SELECT email, datetime_created FROM users;
+
+-- Update a post's content to "Hello to the people of the Earth!" where its initial content is "Hello Earth" by using the record's ID
+UPDATE posts SET content = "Hello to the people of the Earth!" WHERE id = 2;
+
+-- Delete the user with an email of "johndoe@gmail.com"
+DELETE FROM users WHERE email = "johndoe@gmail.com";
