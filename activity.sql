e2c76d449296e1f1dd8a3909faf6b7d411403e1d
Faye <fayebheevayne@gmail.com>
2022-03-16 20:33:03 +0800

    added activity code

--- activity.sql
+++ activity.sql
@@ -1,2 +1,11 @@
 -- Creation of database
 CREATE DATABASE blog_db;
+
+-- Create posts table
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(300) NOT NULL,
+    datetime_created DATETIME NOT NULL
+    PRIMARY KEY (id)  
+);
